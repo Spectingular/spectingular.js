9a080ab94606ddbe6005d4f06d1668d3307f45ec
Spectingular <spectingular@dasberg.nl>
2014-05-13 17:04:02 +0000

    Docs for v0.0.0-build.32+sha.65ff9e7

---
+++ docs/grunt-scripts/spBindOnce.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name sp.binding.spBindOnce
+ *
+ * @description
+ * Directive for handling one way binding, by destroying the scope within.
+ *
+ * @example
+ <example module="example">
+    <file name="index.html">
+       <div ng-controller="ctrl">
+          <h3>one way binding</h3>
+          <span sp-bind-once>
+             value 1: {{model.value1}}<br />
+             value 2: {{model.value2}}
+          </span>
+          <h3>two way binding</h3>
+          <span>
+             value 1: {{model.value1}}<br />
+             value 2: {{model.value2}}
+          </span><br />
+          <h3>change</h3>
+          value 1: <input type="text" ng-model="model.value1" /><br />
+          value 2: <input type="text" ng-model="model.value2" />
+       </div>
+    </file>
+
+    <file name="scripts.js">
+       angular.module('example', ['sp.binding']).
+          controller('ctrl', function($scope) {
+             $scope.model = {
+                value1 : 'original value 1',
+                value2 : 'original value 2'
+             };
+          });
+    </file>
+ </example>
+ **/
+
+angular.module('sp.binding', []).directive('spBindOnce', ['$timeout', function ($timeout) {
+    return {
+        scope: true,
+        link: function (scope) {
+            $timeout(function () {
+                scope.$destroy();
+            }, 0);
+        }
+    };
+}]);
---
+++ docs/js/docs-setup.js
@@ -0,0 +1,25 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Reference"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "sp.binding.spBindOnce",
+      "shortName": "sp.binding.spBindOnce",
+      "type": "directive",
+      "moduleName": "sp.binding",
+      "shortDescription": "Directive for handling one way binding, by destroying the scope within.",
+      "keywords": "$scope angular api binding controller ctrl destroying directive example function handling html js model module ng-controller ng-model original scope scripts sp sp-bind-once spbindonce text type value1 value2"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "spBindOnce.js"
+  ]
+};
