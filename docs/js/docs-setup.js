5c8629789a9e18535abd38b59f9869c0613b0956
Spectingular <spectingular@dasberg.nl>
2014-05-19 09:15:07 +0000

    Docs for v0.0.0-build.37+sha.fb4415a

--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -20,6 +20,33 @@ NG_DOCS={
       "moduleName": "sp.binding",
       "shortDescription": "Directive for handling one way binding, by destroying the scope within.",
       "keywords": "$scope angular api binding controller ctrl destroying directive function handling html js model module ng-controller ng-model original scope scripts sp sp-bind-once spbindexample text type value1 value2"
+    },
+    {
+      "section": "api",
+      "id": "sp.i18n.spProperties",
+      "shortName": "sp.i18n.spProperties",
+      "type": "service",
+      "moduleName": "sp.i18n",
+      "shortDescription": "Service that allows you to provide multi lingual support for properties that can be used",
+      "keywords": "$scope add allows angular api config controller criteria ctrl dutch een en-us english enproperty example function html i18n identifier js key labels lingual locale localeidentifier matching method module multi ng-controller nl-nl nlproperty properties property provide scripts service sp spectingular spproperties sppropertiesexample sppropertiesprovider support values valuesthe"
+    },
+    {
+      "section": "api",
+      "id": "sp.i18n.spPropertiesProvider",
+      "shortName": "sp.i18n.spPropertiesProvider",
+      "type": "service",
+      "moduleName": "sp.i18n",
+      "shortDescription": "Provider that allows you to add properties for a given identifier and locale.",
+      "keywords": "add adds allows api i18n identifier key local locale localeidentifier matching method properties property provider service sp sppropertiesprovider"
+    },
+    {
+      "section": "api",
+      "id": "sp.utility.spUtils",
+      "shortName": "sp.utility.spUtils",
+      "type": "service",
+      "moduleName": "sp.utility",
+      "shortDescription": "Service that provides utility functions such as safe object traversal.",
+      "keywords": "$scope angular api array bar baz controller ctrl default foo function functions html js method module ng-controller obj object path restaurant result resultwithdefault resultwithoutdefault safe scripts service somedefaultvalue sp sputils sputilsexample string traversal traverse traverseordefault utility var"
     }
   ],
   "apis": {
