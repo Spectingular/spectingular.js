574fe903146c86349f4977c0b029921bfc3a106a
Alessandro Vermeulen <alessandro@Mythradil.local>
2014-05-19 10:24:21 +0200

    Utility service

---
+++ src/sp/utility/spUtils.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('sp.utility', []).service('spUtils', function () {
+    var fn = {};
+
+    /**
+     * @param object The object to traverse
+     * @param path Array of string
+     */
+    function traverse(object, path) {
+        if (path.length === 0) {
+            return object;
+        } else {
+            var head  = path.shift(),
+                child = object[head];
+            return angular.isDefined(child) ? traverse(child, path) : undefined;
+        }
+    }
+
+    fn.traverse = traverse;
+
+    fn.traverseOrDefault = function (object, path, defaultValue) {
+        var result = fn.traverse(object, path);
+        return angular.isDefined(result) ? result : defaultValue;
+    };
+
+    return fn;
+});
