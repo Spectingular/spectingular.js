5c8bda5e3b64fead05456009703616cbc8eb3d34
Mischa Dasberg <mischa@dasberg.nl>
2014-05-24 09:48:47 +0200

    Added tests for spKeyBinderConfig

---
+++ test/unit/sp/utility/spKeyBinderConfigSpec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+describe('spKeyBinderConfig provider', function () {
+    var service, document;
+
+    angular.module("defaultConfig", ['sp.utility']);
+
+    angular.module("overrideConfig", ['sp.utility']).
+        config(function (spKeyBinderConfigProvider) {
+            spKeyBinderConfigProvider.setDefaultTarget('x');
+            spKeyBinderConfigProvider.setDefaultType('keyup');
+        });
+
+    describe('using default options', function () {
+        beforeEach(function () {
+            module('defaultConfig');
+            inject(function (spKeyBinderConfig, $document) {
+                service = spKeyBinderConfig;
+                document = $document
+            });
+        });
+
+        it('should get the default options', function () {
+            expect(service.defaultOptions().target).toBe(document);
+            expect(service.defaultOptions().type).toBe('keydown');
+        });
+    })
+
+    describe('using default options', function () {
+        beforeEach(function () {
+            module('overrideConfig');
+            inject(function (spKeyBinderConfig) {
+                service = spKeyBinderConfig;
+            });
+        });
+
+        it('should get the override options', function () {
+            expect(service.defaultOptions().target).toBe('x');
+            expect(service.defaultOptions().type).toBe('keyup');
+        });
+    })
+
+});
