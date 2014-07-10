9afa774e35f29066cd24140b36f4d7c6212b4356
Spectingular <spectingular@dasberg.nl>
2014-07-10 11:51:24 +0000

    Docs for v0.0.0-build.84+sha.7372516

--- docs/grunt-scripts/spectingular.js
+++ docs/grunt-scripts/spectingular.js
@@ -52,353 +52,6 @@ angular.module('sp.binding').directive('spBindOnce', ['$timeout', function ($tim
         }
     };
 }]);
-"use strict";
-
-/**
- * @ngdoc service
- * @name sp.binding.spKeyBinder
- * @requires sp.binding.spKeyBinderConfig
- *
- * @description
- * The `spKeyBinder` service is a utility service that facilitates the binding and unbinding
- * of key combination events to dom elements. It makes it possible to bind multiple key combinations to an key type and on multiple
- * elements.
- *
- * # General usage
- * The bind and unbind functions of the `spKeyBinder` service has:
- *
- * - one mandatory argument — the key combination
- * - two optional arguments
- *   - **`callback`** - a function that is triggered when the key combination on the element is entered
- *   - **`options`** - a configuration object that makes it possible to override the default options
- *
- * The `spKeyBinder` service is provided with default options from the `spKeyBinderConfig` provider.
- * By default the event type is `keydown`.
- *
- * The `spKeyBinder` can also be used without any key combination. If now key combination (undefined) is provided when binding, the key combination will be
- * **`mousedown`**.
- *
- * @example
- <example module="spKeyBinderExample">
- <file name="index.html">
- <div ng-controller="ctrl">
- <span>Pressing the escape key should trigger a broadcast event</span><br />
- <input type="text" id="x" placeholder="key combination ctrl+shift+x should trigger a broadcast event"/><br />
- <div id="y">Clicking here should trigger a broadcast event</div><br /><br />
- <h1>events</h1>
- <ul>
- <li ng-repeat="event in model.events track by $index">{{event}}</li>
- </ul>
- </div>
- </file>
-
- <file name="scripts.js">
- angular.module("spKeyBinderExample", ['sp.binding']).
- controller('ctrl', function($scope, spKeyBinder) {
-             spKeyBinder.bind('escape');
-             spKeyBinder.bind('ctrl+shift+x', function() {
-                $scope.model.events.push('keydown-ctrl+shift+x');
-                $scope.$apply();
-             }, {
-                target: 'x',
-                type: 'keydown'
-             });
-             spKeyBinder.bind(undefined, undefined, {
-                target: 'y',
-                type: 'click'
-             });
-
-             $scope.model = {events: []};
-             $scope.$on('keydown-escape', function(event) {
-                $scope.model.events.push('keydown-escape');
-                $scope.$apply();
-             });
-             $scope.$on('click', function(event) {
-                $scope.model.events.push('click');
-                $scope.$apply();
-             });
-          });
- </file>
- </example>
- */
-angular.module('sp.binding').factory('spKeyBinder', ['$rootScope', '$document', 'spKeyBinderConfig', function ($rootScope, $document, spKeyBinderConfig) {
-    var handlers = {};
-
-    /**
-     * Execute the callback with the correct information.
-     * @param keyCombination The key combination to which the event will be bound.
-     * @param options The options that need to be merged with the default options.
-     * @param callback The callback function.
-     */
-    function execute(keyCombination, options, callback) {
-        options = angular.extend({}, spKeyBinderConfig.defaultOptions(), options); // extend the options
-        var element = angular.isString(options.target) ? angular.element(document.querySelector('#' + options.target)) : options.target;
-
-
-        // check if a key combination was specified.
-        if (angular.isUndefined(keyCombination)) {
-            keyCombination = 'mousedown';
-        }
-
-        callback(keyCombination, options, element);
-    }
-
-    /**
-     * @ngdoc method
-     * @name sp.binding.spKeyBinder#bind
-     * @methodOf sp.binding.spKeyBinder
-     * @description Bind the key combination to the given target using the given options.
-     * @param {string} keyCombination The key combination.
-     * @param {function=} callback The callback that is executed when the entered key combination matches. If no callback function is provided, an event will be broadcast.
-     * @param {Object=} options The options.
-     */
-    function bind(keyCombination, callback, options) {
-        execute(keyCombination, options, function (keyCombination, options, element) {
-            var target = options.target === $document ? 'document' : options.target;
-            var bind = false;
-
-            // check for type
-            if (angular.isUndefined(handlers[options.type])) { // no handler for the given type has been registered yet
-                handlers[options.type] = {
-                    count: 0 // total number of bind registrations
-                };
-                handlers[options.type].elements = {};
-            }
-
-            // check for element
-            if (angular.isUndefined(handlers[options.type].elements[target])) {
-                bind = true;
-                handlers[options.type].elements[target] = {
-                    count: 0,
-                    keyCombinations: {}
-                };
-            }
-
-            // check for key combination
-            if (angular.isUndefined(handlers[options.type].elements[target].keyCombinations[keyCombination])) {
-                handlers[options.type].count++;
-                handlers[options.type].elements[target].count++;
-                handlers[options.type].elements[target].keyCombinations[keyCombination] = {}
-                handlers[options.type].elements[target].keyCombinations[keyCombination].count = 1;
-            } else {
-                handlers[options.type].count++;
-                handlers[options.type].elements[target].count++;
-                handlers[options.type].elements[target].keyCombinations[keyCombination].count++;
-            }
-            handlers[options.type].elements[target].keyCombinations[keyCombination].callback = callback;
-
-            if (bind) {
-                element.on(options.type, function (event) { // do the actual binding
-                    var origin = angular.isDefined(event.delegateTarget.id) ? event.delegateTarget.id : 'document';
-                    var keyCombinations = handlers[options.type].elements[origin].keyCombinations;
-
-                    for (var kc in keyCombinations) {
-                        var keys = kc.split("+");
-                        // check the pressed modifiers
-                        var modifiers = {
-                            shift: keys.indexOf('shift') > -1,
-                            ctrl: keys.indexOf('ctrl') > -1,
-                            alt: keys.indexOf('alt') > -1
-                        };
-
-                        // remove modifiers
-                        if( keys.indexOf('shift') > -1) {
-                            keys.splice(keys.indexOf('shift'), 1)
-                        }
-                        if( keys.indexOf('ctrl') > -1) {
-                            keys.splice(keys.indexOf('ctrl'), 1)
-                        }
-                        if( keys.indexOf('alt') > -1) {
-                            keys.splice(keys.indexOf('alt'), 1)
-                        }
-
-                        var keyCode = event.keyCode ? event.keyCode : event.which ? event.which : undefined;
-                        var character = String.fromCharCode(keyCode).toLowerCase();
-                        var specialKey = spKeyBinderConfig.specialKeys()[keys];
-
-                        // broadcast the event if the key combination matches
-                        if ((modifiers.shift === (event.shiftKey ? true : false)) && // do we require shift and is it pressed?
-                            (modifiers.ctrl === (event.ctrlKey ? true : false)) && // do we require ctrl and is it pressed?
-                            (modifiers.alt === (event.altKey ? true : false)) && // do we require alt and is it pressed?
-                            (
-                                keys.indexOf(character) > -1 || // does the character match
-                                    (angular.isDefined(specialKey) && specialKey === keyCode)) // or does it match a special key
-                            ) {
-
-                            if(angular.isDefined(handlers[options.type].elements[target].keyCombinations[kc].callback)) {
-                                handlers[options.type].elements[target].keyCombinations[kc].callback();
-                            } else {
-                                var eventName = event.type;
-                                // if we have a defined a key combination append it to the event name.
-                                if (keyCode !== 1) {
-                                    eventName = eventName + '-' + kc;
-                                }
-                                $rootScope.$broadcast(eventName);
-                            }
-                        }
-                    }
-                });
-            }
-        });
-    }
-
-    /**
-     * @ngdoc method
-     * @name sp.binding.spKeyBinder#unbind
-     * @methodOf sp.binding.spKeyBinder
-     * @description Unbind the event to the given target for the given key combination.
-     * @param {string} keyCombination The key combination.
-     * @param {Object=} options The options.
-     */
-    function unbind(keyCombination, options) {
-        execute(keyCombination, options, function (keyCombination, options, element) {
-            var target = options.target === $document ? 'document' : options.target;
-
-            if (angular.isDefined(handlers[options.type]) &&
-                angular.isDefined(handlers[options.type].elements[target]) &&
-                angular.isDefined(handlers[options.type].elements[target].keyCombinations[keyCombination].count)) {
-
-                handlers[options.type].elements[target].keyCombinations[keyCombination].count--;
-                handlers[options.type].elements[target].count--;
-                handlers[options.type].count--;
-
-                if (handlers[options.type].elements[target].keyCombinations[keyCombination].count === 0) {
-                    delete handlers[options.type].elements[target].keyCombinations[keyCombination];
-                }
-                if (handlers[options.type].elements[target].count === 0) {
-                    delete handlers[options.type].elements[target];
-                    element.off(options.type);
-                }
-                if (handlers[options.type].count === 0) {
-                    delete  handlers[options.type];
-                }
-
-            }
-        });
-    }
-
-    return {
-        handlers: handlers,
-        bind: bind,
-        unbind: unbind
-    };
-}]);
-"use strict";
-
-/**
- * @ngdoc service
- * @name sp.binding.spKeyBinderConfig
- *
- * @description
- * Service that provides the default options for the spKeyBinder. It also allows you to override
- * the defaults.
-
- */
-angular.module('sp.binding').provider('spKeyBinderConfig',function () {
-    /**
-     * @ngdoc service
-     * @name sp.binding.spKeyBinderConfigProvider
-     *
-     * @description
-     * Provider that allows you to override default options.
-     */
-    this.defaultOptions = {
-        'type': 'keydown'
-    };
-
-    this.specialKeys = {
-        'mousedown': 1,
-        'backspace': 8,
-        'tab': 9,
-        'enter': 13,
-        'break': 19,
-        'capslock': 20,
-        'escape': 27,
-        'space': 32,
-        'pageup': 33,
-        'pagedown': 34,
-        'end': 35,
-        'home': 36,
-        'left': 37,
-        'up': 38,
-        'right': 39,
-        'down': 40,
-        'insert': 45,
-        'delete': 46,
-        'numlock': 144,
-        'scroll': 145,
-        'f1': 112,
-        'f2': 113,
-        'f3': 114,
-        'f4': 115,
-        'f5': 116,
-        'f6': 117,
-        'f7': 118,
-        'f8': 119,
-        'f9': 120,
-        'f10': 121,
-        'f11': 122,
-        'f12': 123
-    };
-    /**
-     * @ngdoc method
-     * @name sp.binding.spKeyBinderConfigProvider#setDefaultTarget
-     * @methodOf sp.binding.spKeyBinderConfigProvider
-     *
-     * @description
-     * Override the default target to which the spKeyBinder will register the events.
-     * @param {String} target The target.
-     */
-    this.setDefaultTarget = function (target) {
-        this.defaultOptions.target = target;
-    };
-    /**
-     * @ngdoc method
-     * @name sp.binding.spKeyBinderConfigProvider#setDefaultType
-     * @methodOf sp.binding.spKeyBinderConfigProvider
-     *
-     * @description
-     * Override the default type to which the event will be bound.
-     * @param {String} type The type
-     */
-    this.setDefaultType = function (type) {
-        this.defaultOptions.type = type;
-    }
-
-    this.$get = ['$document', function ($document) {
-        var defaultOptions = this.defaultOptions;
-        var specialKeys = this.specialKeys;
-        if (angular.isUndefined(defaultOptions.target)) {
-            defaultOptions.target = $document; // set the default target to $document.
-        }
-        return {
-            /**
-             * @ngdoc method
-             * @name sp.binding.spKeyBinderConfig#defaultOptions
-             * @methodOf sp.binding.spKeyBinderConfig
-             *
-             * @description
-             * Gets the spKeyBinder default options
-             * @returns {Object} defaultOptions The default options
-             */
-            defaultOptions: function () {
-                return defaultOptions;
-            },
-            /**
-             * @ngdoc method
-             * @name sp.binding.spKeyBinderConfig#specialKeys
-             * @methodOf sp.binding.spKeyBinderConfig
-             *
-             * @description
-             * Gets the spKeyBinder special keys
-             * @returns {Object} specialKeys The special keys
-             */
-            specialKeys: function () {
-                return specialKeys;
-            }
-        }
-    }];
-})
 'use strict';
 
 /**
@@ -406,7 +59,7 @@ angular.module('sp.binding').provider('spKeyBinderConfig',function () {
  * @name sp.i18n.spProperties
  *
  * @description
- * Service that allows you to provide multi lingual support for properties that can be used
+ * Provider that allows you to provide multi lingual support for properties that can be used
  * for labels etc.
  *
  * @example
@@ -507,6 +160,147 @@ angular.module('sp.i18n').provider('spProperties', function () {
                 return spUtils.traverse(propertyStore, [identifier, localeIdentifier]);
             }
         };
+    }];
+});
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name sp.i18n.directive:spProperty
+ *
+ * @description
+ * Directive for displaying properties from the {@link sp.i18n.spProperties} service.
+ * Note: this directive also support binding with the curly braches notation ( {{  }} )
+ *
+ * @param {String} key The property key to display
+ * @param {String=} identifier The properties identifier
+ * @param {String=} locale The locale
+ *
+ * @example
+ <example module="spPropertyExample">
+    <file name="index.html">
+       <div ng-controller="ctrl">
+          <h3>Using attributes identifier [`example`], key [`welcome-message`] and locale [`nl-nl`]</h3>
+          <div sp-property identifier='example' key='welcome-message' locale='nl-nl'></div>
+          <h3>Using attributes identifier [`example`], key [`welcome-message`] and the default locale</h3>
+          <div sp-property identifier='example' key='welcome-message'></div>
+          <h3>Using attribute key [`welcome-message`] and the default locale and identifier</h3>
+          <div sp-property key='welcome-message'></div>
+       </div>
+    </file>
+
+    <file name="scripts.js">
+       angular.module('spPropertyExample', ['sp.i18n']).
+          config(function(spPropertiesProvider, spPropertyConfigProvider) {
+             spPropertiesProvider.add('example', {
+                'name': 'spectingular',
+                'welcome-message': 'hello I am {{who}}'
+             }, 'en-us');
+             spPropertiesProvider.add('example', {
+                'name': 'spectingular',
+                'welcome-message': 'hallo ik ben {{who}}'
+             }, 'nl-nl');
+             spPropertyConfigProvider.setDefaultIdentifier('example');
+          }).
+          controller('ctrl', function($scope) {
+             $scope.who = 'Spectingular';
+          });
+    </file>
+ </example>
+ **/
+angular.module('sp.i18n').directive('spProperty', ['spProperties', 'spPropertyConfig', '$compile', function (spProperties, spPropertyConfig, $compile) {
+    return {
+        restrict: 'EA',
+        scope: false,
+        link: function (scope, element, attrs) {
+            var identifier = angular.isDefined(attrs.identifier) ? attrs.identifier : spPropertyConfig.defaultIdentifier;
+            var locale = angular.isDefined(attrs.locale) ? attrs.locale : spPropertyConfig.defaultLocale;
+
+            element.html(spProperties.property(identifier, attrs.key, locale));
+            $compile(element.contents())(scope);
+        }
+    };
+}]);
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name sp.i18n.spPropertyConfig
+ *
+ * @description
+ * Provider that provides the default options for the spProperty directive.
+ * It also allows you to override the defaults.
+ *
+ * @Usage
+ * angular.module('myModule', []).config(function(spPropertyConfigProvider) {
+ *      spPropertyConfigProvider.setDefaultIdentifier('example');
+ *  })
+ */
+angular.module('sp.i18n').provider('spPropertyConfig', function () {
+    /**
+     * @ngdoc service
+     * @name sp.i18n.spPropertyConfigProvider
+     *
+     * @description
+     * Provider that allows you to override default options.
+     */
+    this.defaultOptions = {
+        identifier: undefined
+    };
+
+    /**
+     * @ngdoc method
+     * @name sp.i18n.spPropertyConfigProvider#setDefaultIdentifier
+     * @methodOf sp.i18n.spPropertyConfigProvider
+     *
+     * @description
+     * Override the default identifier for which the spProperty will get the properties
+     * @param {String} identifier The identifier.
+     */
+    this.setDefaultIdentifier = function (identifier) {
+        this.defaultOptions.identifier = identifier;
+    };
+
+    /**
+     * @ngdoc method
+     * @name sp.i18n.spPropertyConfigProvider#setDefaultLocale
+     * @methodOf sp.i18n.spPropertyConfigProvider
+     *
+     * @description
+     * Override the default locale for which the spProperty will get the properties
+     * @param {String} locale The locale.
+     */
+    this.setDefaultLocale = function (locale) {
+        this.defaultOptions.locale = locale;
+    };
+
+    this.$get = ['$locale', function ($locale) {
+        var defaultOptions = this.defaultOptions;
+        if (angular.isUndefined(defaultOptions.locale)) {
+            defaultOptions.locale = $locale.id
+        }
+        return {
+            /**
+             * @ngdoc method
+             * @name sp.i18n.spPropertyConfig#defaultIdentifier
+             * @methodOf sp.i18n.spPropertyConfig
+             *
+             * @description
+             * Gets the default identifier
+             * @returns {Object} defaultIdentifier The default identifier
+             */
+            defaultIdentifier: defaultOptions.identifier,
+            /**
+             * @ngdoc method
+             * @name sp.i18n.spPropertyConfig#defaultLocale
+             * @methodOf sp.i18n.spPropertyConfig
+             *
+             * @description
+             * Gets the default locale
+             * @returns {Object} defaultLocale The default locale
+             */
+            defaultLocale: defaultOptions.locale
+        }
     }];
 });
 'use strict';
--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -23,30 +23,12 @@ NG_DOCS={
     },
     {
       "section": "api",
-      "id": "sp.binding.spKeyBinder",
-      "shortName": "sp.binding.spKeyBinder",
-      "type": "service",
-      "moduleName": "sp.binding",
-      "shortDescription": "The spKeyBinder service is a utility service that facilitates the binding and unbinding",
-      "keywords": "$apply $index $on $scope angular api argument arguments bind binding broadcast callback click combination combinations configuration controller ctrl default dom element elements entered escape event events executed facilitates function functions general html js key keycombination keydown keydown-ctrl keydown-escape mandatory matches method model module mousedown multiple ng-controller ng-repeat object optional options override placeholder provided provider push scripts service sp spkeybinder spkeybinderconfig spkeybinderexample target text track trigger triggered type unbind unbinding undefined usage utility"
-    },
-    {
-      "section": "api",
-      "id": "sp.binding.spKeyBinderConfig",
-      "shortName": "sp.binding.spKeyBinderConfig",
-      "type": "service",
-      "moduleName": "sp.binding",
-      "shortDescription": "Service that provides the default options for the spKeyBinder. It also allows you to override",
-      "keywords": "allows api binding default defaultoptions defaults method options override service sp special specialkeys spkeybinder spkeybinderconfig"
-    },
-    {
-      "section": "api",
-      "id": "sp.binding.spKeyBinderConfigProvider",
-      "shortName": "sp.binding.spKeyBinderConfigProvider",
-      "type": "service",
-      "moduleName": "sp.binding",
-      "shortDescription": "Provider that allows you to override default options.",
-      "keywords": "allows api binding bound default event events method options override provider register service setdefaulttarget setdefaulttype sp spkeybinder spkeybinderconfigprovider target type"
+      "id": "sp.i18n.directive:spProperty",
+      "shortName": "spProperty",
+      "type": "directive",
+      "moduleName": "sp.i18n",
+      "shortDescription": "Directive for displaying properties from the sp.i18n.spProperties service.",
+      "keywords": "$scope add angular api attribute attributes ben binding braches config controller ctrl curly default directive display displaying en-us example function hallo hello html i18n identifier ik js key locale module ng-controller nl-nl notation note properties property scripts service setdefaultidentifier sp sp-property spectingular spproperties sppropertiesprovider sppropertyconfigprovider sppropertyexample support welcome-message"
     },
     {
       "section": "api",
@@ -54,8 +36,8 @@ NG_DOCS={
       "shortName": "sp.i18n.spProperties",
       "type": "service",
       "moduleName": "sp.i18n",
-      "shortDescription": "Service that allows you to provide multi lingual support for properties that can be used",
-      "keywords": "$scope add allows angular api config controller criteria ctrl dutch een en-us english enproperty example function html i18n identifier js key labels lingual locale localeidentifier matching method module multi ng-controller nl-nl nlproperty properties property provide scripts service sp spectingular spproperties sppropertiesexample sppropertiesprovider support values valuesthe"
+      "shortDescription": "Provider that allows you to provide multi lingual support for properties that can be used",
+      "keywords": "$scope add allows angular api config controller criteria ctrl dutch een en-us english enproperty example function html i18n identifier js key labels lingual locale localeidentifier matching method module multi ng-controller nl-nl nlproperty properties property provide provider scripts service sp spectingular spproperties sppropertiesexample sppropertiesprovider support values valuesthe"
     },
     {
       "section": "api",
@@ -65,6 +47,24 @@ NG_DOCS={
       "moduleName": "sp.i18n",
       "shortDescription": "Provider that allows you to add properties for a given identifier and locale.",
       "keywords": "add adds allows api i18n identifier key local locale localeidentifier matching method properties property provider service sp sppropertiesprovider"
+    },
+    {
+      "section": "api",
+      "id": "sp.i18n.spPropertyConfig",
+      "shortName": "sp.i18n.spPropertyConfig",
+      "type": "service",
+      "moduleName": "sp.i18n",
+      "shortDescription": "Provider that provides the default options for the spProperty directive.",
+      "keywords": "allows angular api config default defaultidentifier defaultlocale defaults directive example i18n identifier locale method module mymodule options override provider service setdefaultidentifier sp spproperty sppropertyconfig sppropertyconfigprovider"
+    },
+    {
+      "section": "api",
+      "id": "sp.i18n.spPropertyConfigProvider",
+      "shortName": "sp.i18n.spPropertyConfigProvider",
+      "type": "service",
+      "moduleName": "sp.i18n",
+      "shortDescription": "Provider that allows you to override default options.",
+      "keywords": "allows api default i18n identifier locale method options override properties provider service setdefaultidentifier setdefaultlocale sp spproperty sppropertyconfigprovider"
     },
     {
       "section": "api",
