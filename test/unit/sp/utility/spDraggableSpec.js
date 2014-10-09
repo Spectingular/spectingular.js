86649c172bf815b7a432118b53b9e322fe841c3f
Remco Vlierman <remco.vlierman@ing.nl>
2014-10-09 15:09:31 +0200

    added testfile for unitdraggable test

---
+++ test/unit/sp/utility/spDraggableSpec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('utilities', function () {
+    var utils;
+
+    beforeEach(function () {
+        module('sp.utility');
+
+    });
+
+    describe('draggable', function () {
+
+
+        describe('', function () {
+
+        });
+
+
+    })
+});
